e0d5bae9cff958ab2146a4b9269375fcbc355e0b
Adrien Kamran <adrien.kamran@gmail.com>
2021-01-21 13:56:26 -0500

    Static and template files created, links for registration and login created

--- ERP/sample/views.py
+++ ERP/sample/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "landing.html")
 
+
+def login(request):
+    return render(request, "login.html")
+
+
+def register(request):
+    return render(request, "register.html")
