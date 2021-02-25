c05d2f8576f6f5c556ed4d37a54fe0c896c29819
Cedrik Dubois <cedrikdubois.edu@gmail.com>
2021-02-25 08:51:06 -0500

    [ERP-31] Depending on user level, certain navigation buttons appear

--- ERP/dashboard/templatetags/filters.py
+++ ERP/dashboard/templatetags/filters.py
@@ -2,14 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='calculateCost')
-def calculate_cost(value, args):
-    string_args = str(args)
-    args_list = string_args.split(",")
-    arg1 = int(args_list[0])
-    #arg2 = float(args_list[1])
-    return value
-
-@register.filter(name='addString')
-def add_string(value, arg):
-    return f"{value}, {arg}"
+@register.filter(name='group')
+def group(u, group_names):
+    group_names = group_names.split(',')
+    return u.groups.filter(name__in=group_names).exists()
