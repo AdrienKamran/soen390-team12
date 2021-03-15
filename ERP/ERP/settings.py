64c8889b9929e4c96d756ec82a796195ae24bc5b
immaroot <killiankelly@gmail.com>
2021-03-14 20:03:04 -0400

    added customer and salesorder model

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'dashboard',
     'utils',
     'inventory',
+    'sales',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ERP/sales/models.py
+++ ERP/sales/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from inventory.models import Product, Warehouse
+
+class Customer(models.Model):
+    type_choice = (
+        ('company', 'company'),
+        ('individual', 'individual')
+    )
+    name = models.TextField(max_length=256)
+    type = models.TextField(choices=type_choice)
+    email = models.EmailField()
+    phone_number = models.TextField(max_length=256)
+    address_line = models.TextField(max_length=256)
+    city = models.TextField(max_length=256)
+    state = models.TextField(max_length=256)
+    zip_code = models.TextField(max_length=256)
+    country = models.TextField(max_length=256)
+
+class SalesOrder(models.Model):
+    status_choice = (
+        ('PENDING', 'PENDING'),
+        ('SHIPPED', 'SHIPPED'),
+        ('RECEIVED', 'RECEIVED')
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    delivery_date = models.DateTimeField()
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(max_length=10)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.PROTECT)
+    sale_total = models.FloatField()
+    status = models.TextField(choices=status_choice)
