d7a27efc2612e5f1c0c681486c68b784ed5db7f3
AbdulSir <abd.sirawan@hotmail.com>
2021-03-30 00:36:01 -0400

    [ERP-53] Tab3 which is responsible for replenishing vendors inventory is functional now

--- ERP/vendors/forms.py
+++ ERP/vendors/forms.py
@@ -1,7 +1,28 @@
 from django import forms
+from django.forms import ModelChoiceField
 from inventory.models import *
+
+#To render the vendor names in ReplenishMaterialForm
+class VendorModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.v_name
+
+#To render the part names in ReplenishMaterialForm
+class PartModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.p_name
 
 class VendorForm(forms.ModelForm):
     class Meta:
         model = Vendor
         fields = ['v_name', 'v_price_multiplier', 'v_address', 'v_city', 'v_province', 'v_postal_code']
+
+class ReplenishMaterialForm(forms.Form):
+    vendor = VendorModelChoiceField(label='vendor Name', queryset=None)
+    part = PartModelChoiceField(label='part name', queryset=None)
+    quantity = forms.IntegerField(label='Quantity')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['vendor'].queryset = Vendor.objects.all()
+        self.fields['part'].queryset = Part.objects.filter(p_type='Raw Material').all()
