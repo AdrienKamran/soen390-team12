979655b48317b3e8bf2b0c6d6a79b4d557860895
TonyMontana11 <anth.chraim@gmail.com>
2021-04-01 22:39:21 -0400

    added export for accounting

--- ERP/accounting/urls.py
+++ ERP/accounting/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.accounting, name="accounting"),
+    path('download-csv/', views.download_accounting_history, name='download-accounting-history'),
 ]
--- ERP/accounting/views.py
+++ ERP/accounting/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Transaction
+import csv, io
 
 """
 	View for displaying the accunting tab.
@@ -16,3 +18,18 @@ def accounting(request):
 	}
 
 	return render(request, 'accounting.html', context)
+
+@login_required(login_url='login')
+def download_accounting_history(request):
+    items = Transaction.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="transaction-history.csv"'
+    writer = csv.writer(response, delimiter=',')
+    #writing attributes
+    #Find a way to get the attributes directly from the db
+    writer.writerow(['Date', 'Type', 'Name', 'Serial Number', 'Quantity', 'Balance'])
+
+    #writing data corresponding to attributes
+    for obj in items:
+        writer.writerow([obj.t_date, obj.t_type, obj.t_item_name,  obj.t_serial, obj.t_quantity, obj.t_balance])
+    return response
