df119fae8640318d028d4024248b7a356f839d66
TonyMontana11 <anth.chraim@gmail.com>
2021-04-02 20:21:16 -0400

    added buttons to download on-click

--- ERP/accounting/views.py
+++ ERP/accounting/views.py
@@ -5,19 +5,20 @@ from .models import Transaction
 import csv, io
 
 """
-	View for displaying the accunting tab.
+    View for displaying the accounting tab.
 """
+
+
 @login_required(login_url='login')
 def accounting(request):
+    # query for fetching all the transactions
+    t_query = Transaction.objects.order_by('-t_date').all()
 
-	# query for fetching all the transactions
-	t_query = Transaction.objects.order_by('-t_date').all()
+    context = {
+        't_query': t_query,
+    }
+    return render(request, 'accounting.html', context)
 
-	context = {
-		't_query': t_query,
-	}
-
-	return render(request, 'accounting.html', context)
 
 @login_required(login_url='login')
 def download_accounting_history(request):
@@ -25,11 +26,11 @@ def download_accounting_history(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="transaction-history.csv"'
     writer = csv.writer(response, delimiter=',')
-    #writing attributes
-    #Find a way to get the attributes directly from the db
+    # writing attributes
+    # Find a way to get the attributes directly from the db
     writer.writerow(['Date', 'Type', 'Name', 'Serial Number', 'Quantity', 'Balance'])
 
-    #writing data corresponding to attributes
+    # writing data corresponding to attributes
     for obj in items:
-        writer.writerow([obj.t_date, obj.t_type, obj.t_item_name,  obj.t_serial, obj.t_quantity, obj.t_balance])
+        writer.writerow([obj.t_date, obj.t_type, obj.t_item_name, obj.t_serial, obj.t_quantity, obj.t_balance])
     return response
