aa61187b3f0398a833e959d1206e113adbd9fc21
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2021-04-07 22:57:21 -0400

    Heroku commits

--- ERP/notifications/signals.py
+++ ERP/notifications/signals.py
@@ -4,6 +4,10 @@ from django.core import mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 from .models import EmailNotification, EmailNotificationUser, Subscription, Notification, NotifiableModel
 
 
@@ -11,25 +15,19 @@ from .models import EmailNotification, EmailNotificationUser, Subscription, Noti
 def send_welcome_email(sender, instance, created, **kwargs):
     user = instance
     if created:
-        email_notification = EmailNotification(
-            subject='Welcome to the ERP App',
-            message='Welcome to our new platform',
-        )
-        email_notification.save()
-
-        sent = mail.send_mail(
-            subject=email_notification.subject,
-            message=email_notification.message,
-            from_email='from@example.com',
-            recipient_list=[user.email]
-        )
-
-        email_notification_user = EmailNotificationUser(
-            user=user,
-            sent=sent,
-            email_notification=email_notification
-        )
-        email_notification_user.save()
+        message = Mail(
+        from_email='cedrik.dubois1998@gmail.com',
+        to_emails=user.email,
+        subject='Thank you for creating an account with Double Black Diamond ERP',
+        html_content='Glad to have you with us! :)')
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print("Email didn't work.")
 
 
 def notify_by_email(user, subject, message):
